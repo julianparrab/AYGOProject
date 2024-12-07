633285dc1ced0fbc915bcb1b00aec35e6c1170a3
Julian Parra <julianparrab711@gmail.com>
2024-12-07 00:54:10 -0500

    Ajuste Main y Carpetas

--- script.py
+++ script.py
@@ -20,83 +20,64 @@ if __name__ == "__main__":
   print("[INFO] Extracting arguments")
   parser = argparse.ArgumentParser()
 
-  # hyperparameters sent by the client are passed as command-line arguments to
+  # Hyperparameters
   parser.add_argument("--n_estimators", type=int, default=100)
   parser.add_argument("--random_state", type=int, default=0)
 
-  # Data, model, and output directories
+  # Directorios
   parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
   parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
   parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
   parser.add_argument("--train-file", type=str, default="train-V-1.csv")
   parser.add_argument("--test-file", type=str, default="test-V-1.csv")
-
   args, _ = parser.parse_known_args()
 
-  # Imprime las versiones de scikit-learn y joblib
+  #Versiones de scikit-learn y joblib
   print("Scikit-learn Version:", sklearn.__version__)
   print("Joblib Version:", joblib.__version__)
 
-  # Indica que se están leyendo los datos
-  print("[INFO] Reading data")
-  print()
-
   # Carga los datos de entrenamiento y prueba desde los archivos CSV especificados
+  print("[INFO] Reading data")
   train_df = pd.read_csv(os.path.join(args.train, args.train_file))
   test_df = pd.read_csv(os.path.join(args.test, args.test_file))
 
-  # Obtiene las características (features) de los datos de entrenamiento
+  # Obtiene las columnas
   features = list(train_df.columns)
-
-  # Extrae la última columna (asumiendo que es la etiqueta) y la asigna a la variable 'label'
   label = features.pop(-1)
 
-  # Indica que se están construyendo los conjuntos de entrenamiento y prueba
+  # Conjuntos de entrenamiento y prueba
   print("Building training and testing datasets")
-  print()
-
   X_train = train_df[features]
   X_test = test_df[features]
   y_train = train_df[label]
   y_test = test_df[label]
 
-  print('Column order:')
-  print(features)
-  print()
-
-  print("Label column is: ", label)
-  print()
-  print("---- SHAPE OF TRAINING DATA (85%) ----")
+  print("Training Data")
   print(X_train.shape)
   print(y_train.shape)
   print()
 
-  print("---- SHAPE OF TESTING DATA (15%) ----")
+  print("Testing Data")
   print(X_test.shape)
   print(y_test.shape)
   print()
 
-  print("---- Training RandomForest Model ----")
-  model = RandomForestClassifier(n_estimators=100, 
-  max_depth=2, random_state=0)
+  print("Training RandomForest Model")
+  model = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
   model.fit(X_train, y_train)
-  print()
+
 
   model_path = os.path.join(args.model_dir, "model.joblib")
   joblib.dump(model, model_path)
-  print("Model persisted at " + model_path)
-  print()
+
 
   y_pred_test = model.predict(X_test)
   test_acc = accuracy_score(y_test, y_pred_test)
   test_rep = classification_report(y_test, y_pred_test)
 
-  print()
-  print("---- METRICS RESULTS FOR TESTING DATA ----")
-  print()
+
+  print("RESULTS FOR TESTING DATA")
   print("Total Rows are: ", X_test.shape[0])
   print("[TESTING] Model Accuracy is: ", test_acc)
   print("[TESTING] Testing Report:")
   print(test_rep)
-
-
